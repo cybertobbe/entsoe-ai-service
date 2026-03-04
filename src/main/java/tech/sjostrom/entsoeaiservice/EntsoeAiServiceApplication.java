c6ab62b86c2002fe01f78a90e5837d777ae9e7a2
cybertobbe <tobias.sjostrom@iths.se>
2026-03-04 08:23:17 +0100

    Initial commit

---
+++ src/main/java/tech/sjostrom/entsoeaiservice/EntsoeAiServiceApplication.java
@@ -0,0 +1,13 @@
+package tech.sjostrom.entsoeaiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntsoeAiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntsoeAiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/sjostrom/entsoeaiservice/EntsoeAiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.sjostrom.entsoeaiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntsoeAiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
